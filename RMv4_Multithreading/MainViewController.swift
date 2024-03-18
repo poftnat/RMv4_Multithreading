3d0c0cb03ab24e23c219257d19dd7db9b4c8de5e
Natalia_Poftalnaya <poftnataly@gmail.com>
2024-03-18 22:29:27 +0300

    Ветка Main настроена

---
+++ RMv4_Multithreading/MainViewController.swift
@@ -0,0 +1,12 @@
+// MainViewController.swift
+// Copyright © RoadMap. All rights reserved.
+
+import UIKit
+
+/// Экран входа в приложение
+final class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+    }
+}
